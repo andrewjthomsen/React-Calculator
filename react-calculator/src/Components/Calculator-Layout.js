bfdb814125b76ddf6f2fce83932c9f0c3cf03106
andrew <andrewjthomsen@yahoo.com>
2019-11-25 15:14:01 -0800

    Psuedo coded notes for todos

--- react-calculator/src/Components/Calculator-Layout.js
+++ react-calculator/src/Components/Calculator-Layout.js
@@ -6,7 +6,7 @@ class CalculatorLayout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      display: 0
     }
   }
   render() {
@@ -15,34 +15,34 @@ class CalculatorLayout extends Component {
 <Card bg="dark" text="white" style={{ width: '18rem' }}>
 <Card.Body>
 <Row className="row1">
-<Col sm={12}>0</Col>
+<Col id="display" sm={12}>0</Col>
 </Row>
 <Row className="row2">
-<Col sm={6}>AC</Col>
-<Col sm={3}>/</Col>
-<Col sm={3}>x</Col>
+<Col id="clear" sm={6}>AC</Col>
+<Col id="divide" sm={3}>/</Col>
+<Col id="multiply" sm={3}>x</Col>
 </Row>
 <Row className="row3">
-<Col sm={3}>7</Col>
-<Col sm={3}>8</Col>
-<Col sm={3}>9</Col>
-<Col sm={3}>-</Col>
+<Col id="seven" sm={3}>7</Col>
+<Col id="eight" sm={3}>8</Col>
+<Col id="nine" sm={3}>9</Col>
+<Col id="minus" sm={3}>-</Col>
 </Row>
 <Row className="row4">
-<Col sm={3}>4</Col>
-<Col sm={3}>5</Col>
-<Col sm={3}>6</Col>
-<Col sm={3}>+</Col>
+<Col id="three" sm={3}>4</Col>
+<Col id="five" sm={3}>5</Col>
+<Col id="six" sm={3}>6</Col>
+<Col id="plus"s m={3}>+</Col>
 </Row>
 <Row className="row5">
-<Col sm={3}>1</Col>
-<Col sm={3}>2</Col>
-<Col sm={3}>3</Col>
-<Col sm={3}>=</Col>
+<Col id="one" sm={3}>1</Col>
+<Col id="two" sm={3}>2</Col>
+<Col id="three" sm={3}>3</Col>
+<Col id="equals" sm={3}>=</Col>
 </Row>
 <Row className="row6">
-<Col sm={6}>0</Col>
-<Col sm={3}>.</Col>
+<Col id="zero" sm={6}>0</Col>
+<Col id="decimal" sm={3}>.</Col>
 <Col sm={3}></Col>
 </Row>
 </Card.Body>
@@ -61,3 +61,11 @@ export default CalculatorLayout;
 // row4- 4 columns of 3 rows
 // row5- 4 columns of 3 rows css trick to create equal button behave, even though is part of two rows
 // row6- row 2 exactly - refer to 2
+
+// Functions for logic
+// 1. Create isOperator function to handle +,-,/,x
+// 2. Create function to update display state
+// 3. Create a clear function to clear display
+// 4. Switch case to handle what to do with numbers once operator is chosen
+// 5. Create a function to keep track of user input
+// --> Handles numbers and operation pushed
